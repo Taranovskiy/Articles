dad3f497b92751d2b4b75453fb0a614fa6366b32
Vladimir Taranovskiy <8756136@gmail.com>
2018-12-09 22:51:51 +0300

    add Добавил UserForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ArticleList from './ArticleList';
 import UserForm from './UserForm';
-import Select from 'react-select';
 
 export default class App extends Component {
     static propTypes = {
         articles: PropTypes.array
     }
 
-    state = {
-        selection: null
-    }
-
-    
     render() {
-        const options = this.props.articles.map((article) => {
-            return ({
-                label: article.title,
-                value: article.id
-            });
-        });
-        
+
         return (
             <div>
-                <UserForm/>
-                <Select
-                    options = {options}
-                    value = {this.state.selection}
-                    onChange = {this.changeSelection}
-                    isMulti = {true} />
-                <ArticleList articles = {this.props.articles} />
+                <UserForm articles={this.props.articles} />
+                <ArticleList articles={this.props.articles} />
             </div>
         );
     }
-
-    changeSelection = (selection) => this.setState({selection})
 }
